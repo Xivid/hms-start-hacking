dd60726170d102060f22c1b83ae59db2919a7d45
Zhifei Yang <xividyzf@gmail.com>
2020-09-19 20:50:20 +0200

    Swipe works!

---
+++ app/src/main/java/com/huawei/hackzurich/Profile.kt
@@ -0,0 +1,10 @@
+package com.huawei.hackzurich
+
+
+data class Profile(
+    val age: Int,
+    val distance: Int,
+    val id: Int,
+    val name: String,
+    val profile_pic: String
+)
--- app/src/main/java/com/huawei/hackzurich/SwipeActivity.kt
+++ app/src/main/java/com/huawei/hackzurich/SwipeActivity.kt
@@ -1,11 +1,74 @@
 package com.huawei.hackzurich
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.recyclerview.widget.DefaultItemAnimator
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.yuyakaido.android.cardstackview.*
+import kotlinx.android.synthetic.main.swipe.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class SwipeActivity : AppCompatActivity() {
+class SwipeActivity : AppCompatActivity(), CardStackListener {
+    private val adapter = ProfilesAdapter()
+    private lateinit var layoutManager: CardStackLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fresco.initialize(this)
         setContentView(R.layout.swipe)
+
+        layoutManager = CardStackLayoutManager(this, this).apply {
+            setSwipeableMethod(SwipeableMethod.AutomaticAndManual)
+            setOverlayInterpolator(LinearInterpolator())
+        }
+
+        stack_view.layoutManager = layoutManager
+        stack_view.adapter = adapter
+        stack_view.itemAnimator.apply {
+            if (this is DefaultItemAnimator) {
+                var supportsChangeAnimations = false
+            }
+        }
+
+        TinderAPI().getProfiles().enqueue(object : Callback<List<Profile>> {
+            override fun onFailure(call: Call<List<Profile>>, t: Throwable) {
+
+            }
+
+            override fun onResponse(call: Call<List<Profile>>, response: Response<List<Profile>>) {
+                response.body()?.let {
+                    adapter.setProfiles(it)
+                }
+            }
+        })
+    }
+
+    override fun onCardDisappeared(view: View?, position: Int) {
+
+    }
+
+    override fun onCardDragging(direction: Direction?, ratio: Float) {
+
+    }
+
+    override fun onCardSwiped(direction: Direction?) {
+
+    }
+
+    override fun onCardCanceled() {
+
+    }
+
+    override fun onCardAppeared(view: View?, position: Int) {
+
+    }
+
+    override fun onCardRewound() {
+
     }
 }
