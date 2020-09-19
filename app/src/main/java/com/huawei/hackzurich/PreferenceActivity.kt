62da2ad3e5283266434fab6802f10ef89e93f217
LDA <ling.dai@imtf.com>
2020-09-19 20:57:19 +0200

    Merge branch 'master' of https://github.com/Xivid/hms-start-hacking into master
     Conflicts:
    	app/src/main/res/layout/preference.xml

--- app/src/main/java/com/huawei/hackzurich/PreferenceActivity.kt
+++ app/src/main/java/com/huawei/hackzurich/PreferenceActivity.kt
@@ -1,11 +1,19 @@
 package com.huawei.hackzurich
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.preference.*
 
 class PreferenceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.preference)
+
+        next.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
+
+
 }
