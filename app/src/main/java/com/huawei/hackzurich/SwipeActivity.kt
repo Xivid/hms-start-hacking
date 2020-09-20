aba7572a977d50c11f88c2f126c6d5ed69597d95
Zhifei Yang <xividyzf@gmail.com>
2020-09-20 06:09:08 +0200

    Simple notification

--- app/src/main/java/com/huawei/hackzurich/SwipeActivity.kt
+++ app/src/main/java/com/huawei/hackzurich/SwipeActivity.kt
@@ -1,18 +1,27 @@
 package com.huawei.hackzurich
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.animation.LinearInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.DefaultItemAnimator
 import com.facebook.drawee.backends.pipeline.Fresco
-import com.yuyakaido.android.cardstackview.*
+import com.huawei.hms.push.constant.RemoteMessageConst.Notification.CHANNEL_ID
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager
+import com.yuyakaido.android.cardstackview.CardStackListener
+import com.yuyakaido.android.cardstackview.Direction
+import com.yuyakaido.android.cardstackview.SwipeableMethod
 import kotlinx.android.synthetic.main.swipe.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class SwipeActivity : AppCompatActivity(), CardStackListener {
     private val adapter = ProfilesAdapter()
@@ -50,6 +59,32 @@ class SwipeActivity : AppCompatActivity(), CardStackListener {
 
         imageView6.setOnClickListener {
             setContentView(R.layout.recipesplan)
+            // Create the NotificationChannel, but only on API 26+ because
+            // the NotificationChannel class is new and not in the support library
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name = "testChannel"
+                val descriptionText = "test channel desc"
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+                // Register the channel with the system
+                val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+            var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.c_buoycircle_icon_normal)
+                .setContentTitle("Food Consumption Reminder")
+//                .setContentText("")
+                .setStyle(NotificationCompat.BigTextStyle()
+                    .bigText("Your tomatoes is going to expire in 3 days. Consumed them?"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            val notificationManager = NotificationManagerCompat.from(this)
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(System.currentTimeMillis().toInt(), builder.build())
+
         }
 
     }
